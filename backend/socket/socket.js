4e3d75a33ba59908a915d70520a3884f9089b15a
Blaster1111 <rudrashukla0711@gmail.com>
2024-04-12 22:12:15 +0530

    added import

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from 'http';
 import express from "express";
+import { decodeAuthToken } from "../middleware/protectRoute";
 
 const app = express();
 
