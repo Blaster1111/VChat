fee32678b1d1529da1f6f5522c95f8293364fcfb
Blaster1111 <rudrashukla0711@gmail.com>
2024-04-29 23:34:46 +0530

    modified room socket logic

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,7 @@
     import { decodeAuthToken } from "../middleware/protectRoute.js";
     import Conversation from "../models/conversation.model.js";
     import Message from "../models/message.model.js";
+    import { io } from '../socket/socket.js';
     import { getReceiverSocketId } from "../socket/socket.js";
 
     export const sendMessage = async(req,res)=>{
